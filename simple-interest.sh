059a0629873d6ccd6f72fd93f92084d7fde46434
clashforthecrown <82494042+clashforthecrown@users.noreply.github.com>
2023-09-06 13:22:51 -0600

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -6,7 +6,7 @@
        
        # Author: Upkar Lidder (IBM)
        # Additional Authors:
-       # <your GitHub username>
+       # <clashforthecrown>
        
        # Input:
        # p, principal amount
